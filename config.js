f5a79a6d970f976efe16775298e76bcc86f4c0c5
NidaleNieve <62642672+NidaleNieve@users.noreply.github.com>
2024-04-16 23:56:53 +0000

    updeitaði proxy

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
   // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
-  VITE_CORS_PROXY_URL: "https://curious-tulumba-85bf70.netlify.app",
+  VITE_CORS_PROXY_URL: "https://sensational-rugelach-ea6f9e.netlify.app",
 
   // The READ API key to access TMDB
   VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZmZkOGY0NGRiOGU3NjJmZDE2N2EwOWUxNTFhM2Y5NyIsInN1YiI6IjY1ZTFmMWNmYTM5ZDBiMDE4NjBhMDAwYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8vHMM1Xf5PNkjkO3_IRhezLrU5LHnHfmS3ghR1Exv7Q",
